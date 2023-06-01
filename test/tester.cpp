f676775d55decb2cbcd16f8f8f0c9cd7ba79386e
attilakoksal <attila.b.koksal@gmail.com>
2023-05-31 23:07:08 -0700

    unit tests edited

--- test/tester.cpp
+++ test/tester.cpp
@@ -3,27 +3,27 @@
 #include "../lib/testsubject.h"
 
 
-TEST(testSubjecttests,isEven){
+TEST(TEST1,isEven){
     EXPECT_FALSE(TestSubject::isEven(7));
 }
 
-TEST(testSubjecttests,isEven){
+TEST(TEST2,isEven){
     EXPECT_TRUE(TestSubject::isEven(4));
 }
 
-TEST(testSubjecttests,isEven){
+TEST(TEST3,isEven){
     ASSERT_EQ(TestSubject::isEven(8),true);
 }
 
-TEST(MathTest, Square) {
+TEST(TEST4, Square) {
     ASSERT_EQ(TestSubject::square(2), 4);
     ASSERT_EQ(TestSubject::square(3), 9);
 }
 
-TEST(MathTest, Square) {
+TEST(TEST5, Square) {
     ASSERT_EQ(TestSubject::square(16), 256);
 }
 
-TEST(MathTest, Square) {
+TEST(TEST6, Square) {
     ASSERT_GE(TestSubject::square(0), 0);
 }
